67acb60e4c97f34d721263c1a278d941d97fb5e5
Arafat Tehsin <arafattehsin@hotmail.com>
2018-08-18 14:46:43 +0300

    Added the Sentiment Middleware class.

---
+++ libraries/Bot.Builder.Community.Middleware.SentimentAnalysis/SentimentMiddleware.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bot.Builder.Community.Middleware.SentimentAnalysis
+{
+    public class SentimentMiddleware : IMiddleware
+    {
+        public SentimentMiddleware(IConfiguration configuration)
+        {
+            ApiKey = configuration.GetValue<string>("SentimentKey");
+        }
+
+        public string ApiKey { get; }
+
+        public async Task OnTurn(ITurnContext context, MiddlewareSet.NextDelegate next)
+        {
+            if (context.Activity.Type is ActivityTypes.Message)
+            {
+                context.Services.Add<string>(await context.Activity.Text.Sentiment(ApiKey));
+            }
+
+            await next();
+        }
+
+    }
+}
