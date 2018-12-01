725ef8872c455e586f0c7d4c8da191121144abcc
garypretty <gary@garypretty.co.uk>
2018-12-01 19:20:58 +0000

    Added Alexa Sample

---
+++ samples/Alexa Adapter Sample/Alexa Adapter Sample/Startup.cs
@@ -0,0 +1,109 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Linq;
+using Bot.Builder.Community.Adapters.Alexa.Integration.AspNet.Core;
+using Bot.Builder.Community.Adapters.Alexa.Middleware;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using Microsoft.Bot.Configuration;
+using Microsoft.Bot.Connector.Authentication;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Alexa_Adapter_Sample
+{
+    /// <summary>
+    /// The Startup class configures services and the request pipeline.
+    /// </summary>
+    public class Startup
+    {
+        private ILoggerFactory _loggerFactory;
+        private bool _isProduction = false;
+
+        public Startup(IHostingEnvironment env)
+        {
+            _isProduction = env.IsProduction();
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAlexaBot<AlexaAdapterSampleBot>(options =>
+            {
+                // Set this to true to validate that a request has come from the Alexa
+                // service - this is a requirement for skill certification
+                // disable this if you want to debug using other tools like Postman.
+                options.AlexaOptions.ValidateIncomingAlexaRequests = true;
+
+                // Determine if we should end a session after each turn
+                // If set to true, you can choose to keep the session open
+                // by using the ExpectingInput InputHint in your outgoing activity
+                options.AlexaOptions.ShouldEndSessionByDefault = true;
+
+                ILogger logger = _loggerFactory.CreateLogger<AlexaAdapterSampleBot>();
+
+                // Catches any errors that occur during a conversation turn and logs them.
+                options.AlexaOptions.OnTurnError = async (context, exception) =>
+                {
+                    logger.LogError($"Exception caught : {exception}");
+                    await context.SendActivityAsync("Sorry, it looks like something went wrong.");
+                };
+
+                // This middleware will look for a known slot called 'Phrase'
+                // and transform an incoming IntentRequest with this slot into 
+                // a MessageActivity, using the value of the Phrase slot as the 
+                // Text property for the activity. See the readme for more details
+                // on configuring your Alexa skill for this.
+                options.Middleware.Add(new AlexaIntentRequestToMessageActivityMiddleware());
+            });
+
+            services.AddBot<AlexaAdapterSampleBot>(options =>
+            {
+                var secretKey = Configuration.GetSection("botFileSecret")?.Value;
+                var botFilePath = Configuration.GetSection("botFilePath")?.Value;
+
+                var botConfig = BotConfiguration.Load(botFilePath ?? @".\BotConfiguration.bot", secretKey);
+                services.AddSingleton(sp => botConfig ?? throw new InvalidOperationException($"The .bot config file could not be loaded. ({botConfig})"));
+
+                var environment = _isProduction ? "production" : "development";
+                var service = botConfig.Services.Where(s => s.Type == "endpoint" && s.Name == environment).FirstOrDefault();
+                if (!(service is EndpointService endpointService))
+                {
+                    throw new InvalidOperationException($"The .bot file does not contain an endpoint with name '{environment}'.");
+                }
+
+                options.CredentialProvider = new SimpleCredentialProvider(endpointService.AppId, endpointService.AppPassword);
+
+                ILogger logger = _loggerFactory.CreateLogger<AlexaAdapterSampleBot>();
+
+                options.OnTurnError = async (context, exception) =>
+                {
+                    logger.LogError($"Exception caught : {exception}");
+                    await context.SendActivityAsync("Sorry, it looks like something went wrong.");
+                };
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            _loggerFactory = loggerFactory;
+
+            app.UseDefaultFiles()
+                .UseStaticFiles()
+                .UseBotFramework()
+                .UseAlexa();
+        }
+    }
+}
