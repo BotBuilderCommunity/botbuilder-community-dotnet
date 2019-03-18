f8a1cc3b0744836debad419b583fab201c2fe5a7
Arafat Tehsin <arafattehsin@hotmail.com>
2019-03-18 09:16:17 +0300

    Added the new sample for the Amazon Alexa, Google Assistant and Microsoft Cortana.

---
+++ samples/Cortana Assistant Alexa Sample/Models/ExchangeRates.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cortana_Assistant_Alexa_Sample.Models
+{
+    public class ExchangeRates
+    {
+        public class item
+        {
+            public string key { get; set; }
+
+            public float value { get; set; }
+        }
+
+        public class RootObject
+        {
+            public bool success { get; set; }
+
+            public string terms { get; set; }
+
+            public string privacy { get; set; }
+
+            public int timestamp { get; set; }
+
+            public string source { get; set; }
+
+            public Dictionary<string, double> quotes { get; set; }
+        }
+    }
+}
