7fd5de081a579df3358dcf19d6155f5e4018f351
stevenlivz <steven@livz.org>
2019-08-29 18:00:39 +0100

    Added support for Dialogs

---
+++ libraries/Bot.Builder.Community.Adapters.Alexa/Directives/Dialogs/DialogConfirmIntentDirective.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Bot.Builder.Community.Adapters.Alexa.Directives.Dialogs
+{
+    /// <summary>
+    /// Sends Alexa a command to confirm the all the information the user has provided for the intent before the skill takes action.
+    /// </summary>
+    public class DialogConfirmIntentDirective : DialogDirective
+    {
+        public DialogConfirmIntentDirective(string intent, AlexaConfirmationState confirmationStatus)
+            : base(intent, confirmationStatus) { }
+
+        public string Type => "Dialog.ConfirmIntent";
+    }
+}
---
+++ libraries/Bot.Builder.Community.Adapters.Alexa/Directives/Dialogs/DialogElicitSlotDirective.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Bot.Builder.Community.Adapters.Alexa.Directives.Dialogs
+{
+    /// <summary>
+    /// Sends Alexa a command to ask the user for the value of a specific slot.
+    /// </summary>
+    public class DialogElicitSlotDirective : DialogDirective
+    {
+        public DialogElicitSlotDirective(string intent, string slotToElicit)
+            : base(intent) {
+
+            SlotToElicit = slotToElicit;
+        }
+
+        public string Type => "Dialog.ElicitSlot";
+
+        public string SlotToElicit { get; set; }
+    }
+}
