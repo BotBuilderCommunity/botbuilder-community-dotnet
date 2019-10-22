5097d9203d38c04ae9165c957ee60a30a35bb602
Arafat Tehsin <arafat@outlook.com>
2019-10-22 15:02:43 +1100

    Fixed the Microsoft Licensing text.

--- samples/Sentiment Middleware Sample/AdapterWithErrorHandler.cs
+++ samples/Sentiment Middleware Sample/AdapterWithErrorHandler.cs
@@ -1,7 +1,4 @@
-﻿// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-using System;
+﻿using System;
 using System.Threading.Tasks;
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Connector;
