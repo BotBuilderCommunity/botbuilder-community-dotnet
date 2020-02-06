2565a569d1850b8e4f07fabc44256c88d5522591
matiasroldan6 <matias.roldan@southworks.com>
2020-02-06 11:07:19 -0300

    Add adapter tests

---
+++ tests/Bot.Builder.Community.Adapters.Tests/TwitterAdapterTests.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Bot.Builder.Community.Adapters.Twitter.Webhooks.Models;
+using Castle.Core.Internal;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+using Microsoft.Extensions.Options;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace Bot.Builder.Community.Adapters.Twitter.Tests
+{
+    [TestClass]
+    public class TwitterAdapterTests
+    {
+        private readonly Mock<IOptions<TwitterOptions>> _testOptions = new Mock<IOptions<TwitterOptions>>();
+
+        [TestMethod]
+        [TestCategory("Twitter")]
+        public void ConstructorWithOptionsSucceeds()
+        {
+            Assert.IsNotNull(new TwitterAdapter(_testOptions.Object));
+        }
+
+        [TestMethod]
+        public void UseWithMiddlewareShouldSucceed()
+        {
+            var adapter = new TwitterAdapter(_testOptions.Object);
+            var middleware = new Mock<IMiddleware>();
+            var result = adapter.Use(middleware.Object);
+
+            Assert.IsFalse(result.MiddlewareSet.IsNullOrEmpty());
+        }
+
+        [TestMethod]
+        public async Task ProcessActivityShouldSucceed()
+        {
+            var adapter = new TwitterAdapter(_testOptions.Object);
+            var directMessageEvent = new DirectMessageEvent();
+            var bot = new Mock<IBot>();
+
+            directMessageEvent.MessageText = "test message text";
+            directMessageEvent.Sender = new Webhooks.Models.Twitter.TwitterUser();
+            directMessageEvent.Recipient = new Webhooks.Models.Twitter.TwitterUser();
+            directMessageEvent.Sender.Id = string.Empty;
+            directMessageEvent.Sender.ScreenName = string.Empty;
+            directMessageEvent.Recipient.Id = string.Empty;
+            directMessageEvent.Recipient.ScreenName = string.Empty;
+            bot.SetupAllProperties();
+
+            await adapter.ProcessActivity(directMessageEvent, bot.Object.OnTurnAsync);
+            bot.Verify(b => b.OnTurnAsync(It.IsAny<TurnContext>(), It.IsAny<CancellationToken>()), Times.Once);
+        }
+
+        [TestMethod]
+        public async Task SendActivitiesAsyncShouldReturnEmptyResponsesWithEmptyActivities()
+        {
+            var adapter = new TwitterAdapter(_testOptions.Object);
+            var activity = new Activity();
+
+            using (var turnContext = new TurnContext(adapter, activity))
+            {
+                var result = await adapter.SendActivitiesAsync(turnContext, new Activity[0], default);
+                Assert.IsTrue(result.IsNullOrEmpty());
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NotSupportedException))]
+        public async Task UpdateActivityAsyncShouldReturnNotSupportedException()
+        {
+            var adapter = new TwitterAdapter(_testOptions.Object);
+            var activity = new Activity();
+            
+            using (var turnContext = new TurnContext(adapter, activity))
+            {
+                await Assert.ThrowsExceptionAsync<NotSupportedException>(async () =>
+                {
+                   await adapter.UpdateActivityAsync(turnContext, activity, default);
+                });
+            }
+        }
+    }
+}
