a3838ebb0c6936f4fe2ad085c14bb42a9506ec2b
Gary Pretty <gary@garypretty.co.uk>
2020-02-07 18:39:58 +0000

    Release/070220 (#196)
    **New features**
    
    * Added AdaptiveCardPrompt (#193) - Thanks to Michael Richardson <40401643+mdrichardson@users.noreply.github.com>
    
    **Bug fixes**
    
    * Update Google Content Type
    
    **Other updates**
    
    * Fixed some NuGet settings in project files

---
+++ libraries/Bot.Builder.Community.Dialogs.Prompts/AdaptiveCardPromptResult.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace Bot.Builder.Community.Dialogs.Prompts
+{
+    /// <summary>
+    /// Represents a result from adaptive card input.
+    /// </summary>
+    /// <typeparam name="T">Type returned by recognizer.</typeparam>
+    public class AdaptiveCardPromptResult
+    {
+        /// <summary>
+        /// Gets or sets the Value of the Adaptive Card input.
+        /// </summary>
+        /// <value>
+        /// The value of the user's input from the Adaptive Card.
+        /// </value>
+        public object Data { get; set; }
+
+        /// <summary>
+        /// Gets or sets Error enum.
+        /// </summary>
+        /// <value>
+        /// If not recognized.succeeded, include reason why, if known.
+        /// </value>
+        public AdaptiveCardPromptErrors Error { get; set; }
+
+        /// <summary>
+        /// Gets or sets array of missing required Ids.
+        /// </summary>
+        /// <value>
+        /// Array of requiredIds that were not included with user input.
+        /// </value>
+        public List<string> MissingIds { get; set; }
+    }
+}
