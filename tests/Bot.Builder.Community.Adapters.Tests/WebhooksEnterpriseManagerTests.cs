a5310f2d80c5530e9c5b63e2b029b35b4262c850
Santiago Grangetto <santiagograngetto@gmail.com>
2020-02-10 17:26:53 -0300

    Add tests for WebhooksEnterpriseManager

---
+++ tests/Bot.Builder.Community.Adapters.Tests/WebhooksEnterpriseManagerTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Bot.Builder.Community.Adapters.Twitter.Webhooks.Services;
+using Microsoft.Extensions.Options;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace Bot.Builder.Community.Adapters.Twitter.Tests
+{
+    [TestClass]
+    public class WebhooksEnterpriseManagerTests
+    {
+        private readonly Mock<IOptions<TwitterOptions>> _testOptions = new Mock<IOptions<TwitterOptions>>();
+
+        [TestMethod]
+        public async Task RegisterWebhookWithEmptyUrlShouldFail()
+        {
+            var enterpriseManager = new WebhooksEnterpriseManager(_testOptions.Object.Value);
+
+            await Assert.ThrowsExceptionAsync<ArgumentException>(async () => { await enterpriseManager.RegisterWebhook(""); });
+        }
+    }
+}
