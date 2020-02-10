5906597bdd7065a31629fafef00d8dee8540c37e
Santiago Grangetto <santiagograngetto@gmail.com>
2020-02-10 17:27:01 -0300

    Add tests for WebhookPremiumManager

---
+++ tests/Bot.Builder.Community.Adapters.Tests/WebhooksPremiumManagerTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Bot.Builder.Community.Adapters.Twitter.Webhooks.Services;
+using Microsoft.Extensions.Options;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace Bot.Builder.Community.Adapters.Twitter.Tests
+{
+    [TestClass]
+    public class WebhooksPremiumManagerTests
+    {
+        private readonly Mock<IOptions<TwitterOptions>> _testOptions = new Mock<IOptions<TwitterOptions>>();
+
+        [TestMethod]
+        public async Task RegisterWebhookWithEmptyUrlShouldFail()
+        {
+            var premiumManager = new WebhooksPremiumManager(_testOptions.Object.Value);
+
+            await Assert.ThrowsExceptionAsync<ArgumentException>(async () =>
+            {
+                await premiumManager.RegisterWebhook("", "environment_test");
+            });
+        }
+    }
+}
