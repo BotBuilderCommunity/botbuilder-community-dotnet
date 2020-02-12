d1cd5e0847e6fab265cbf6dfe243ecc2d4a67a68
Santiago Grangetto <santiagograngetto@gmail.com>
2020-02-12 12:20:47 -0300

    Add tests for WebhookHostingService class

---
+++ tests/Bot.Builder.Community.Adapters.Tests/Hosting/WebhookHostingServiceTests.cs
@@ -0,0 +1,33 @@
+﻿using Bot.Builder.Community.Adapters.Twitter.Hosting;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace Bot.Builder.Community.Adapters.Twitter.Tests.Hosting
+{
+    [TestClass]
+    public class WebhookHostingServiceTests
+    {
+        private readonly Mock<ILogger<WebhookHostedService>> _testLogger = new Mock<ILogger<WebhookHostedService>>();
+        private readonly Mock<IOptions<TwitterOptions>> _testOptions = new Mock<IOptions<TwitterOptions>>();
+        private readonly Mock<IApplicationLifetime> _testAppLifetime = new Mock<IApplicationLifetime>();
+
+        [TestMethod]
+        public void StartAsyncShouldComplete()
+        {
+            var service = new WebhookHostedService(_testAppLifetime.Object, _testOptions.Object, _testLogger.Object);
+            var start = service.StartAsync(default);
+            Assert.IsTrue(start.IsCompleted);
+        }
+
+        [TestMethod]
+        public void StopAsyncShouldComplete()
+        {
+            var service = new WebhookHostedService(_testAppLifetime.Object, _testOptions.Object, _testLogger.Object);
+            var stop = service.StopAsync(default);
+            Assert.IsTrue(stop.IsCompleted);
+        }
+    }
+}
