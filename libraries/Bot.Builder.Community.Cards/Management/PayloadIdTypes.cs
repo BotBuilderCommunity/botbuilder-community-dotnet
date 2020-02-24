15e8fbccbaae55323f5aa8954d16788e62f2d7ed
TharosTheDragon <kyle_delaney@msn.com>
2020-02-24 15:46:49 -0800

    Rename PayloadId to PayloadItem

--- libraries/Bot.Builder.Community.Cards/Management/PayloadIdTypes.cs
+++ libraries/Bot.Builder.Community.Cards/Management/PayloadIdTypes.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using Microsoft.Bot.Builder;
 
 namespace Bot.Builder.Community.Cards.Management
 {
@@ -9,12 +10,12 @@ namespace Bot.Builder.Community.Cards.Management
     public static class PayloadIdTypes
     {
         /// <summary>
-        /// An action ID should be globally unique and not found on different actions.
+        /// An action ID should be globally unique and not found in different actions.
         /// </summary>
         public const string Action = "action";
 
         /// <summary>
-        /// A card ID should be the same for every action on a single card.
+        /// A card ID should be the same for every action in a single card attachment.
         /// </summary>
         public const string Card = "card";
 
@@ -22,11 +23,15 @@ namespace Bot.Builder.Community.Cards.Management
         /// A carousel ID should be the same for every action across all card attachments
         /// in a single activity. This is not called an activity ID to avoid confusion because
         /// that would be ambiguous with an activity's actual activity ID from the channel.
+        /// Activities with a "list" attachment layout can still use this.
         /// </summary>
         public const string Carousel = "carousel";
 
         /// <summary>
-        /// A batch ID should be the same for every action in a batch of activities.
+        /// A batch ID should be the same for every action in a "batch" of activities.
+        /// A batch of activities normally refers to the activities sent in a call to
+        /// <see cref="ITurnContext.SendActivitiesAsync">SendActivitiesAsync</see>
+        /// but this can be used for any arbitrarily-defined group of activities.
         /// </summary>
         public const string Batch = "batch";
 
