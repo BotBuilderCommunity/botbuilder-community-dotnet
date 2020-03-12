1130783e77bf526eb475b4fbb7d122b6bb9a76dc
Gary Pretty <gary@garypretty.co.uk>
2020-03-12 18:08:57 +0000

    Update AlexaRequestMapperOptions.cs

--- Bot.Builder.Community.Adapters.Alexa.Core/AlexaRequestMapperOptions.cs
+++ Bot.Builder.Community.Adapters.Alexa.Core/AlexaRequestMapperOptions.cs
@@ -4,7 +4,6 @@ namespace Bot.Builder.Community.Adapters.Alexa.Core
 {
     public class AlexaRequestMapperOptions
     {
-        public string BotId { get; internal set; }
         public string ChannelId { get; internal set; } = "alexa";
         public string DefaultIntentSlotName { get; set; } = "phrase";
         public bool ShouldEndSessionByDefault { get; set; } = true;
