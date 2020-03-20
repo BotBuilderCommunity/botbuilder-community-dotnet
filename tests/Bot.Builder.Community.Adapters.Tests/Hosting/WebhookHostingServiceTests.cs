08a21b9fe13cfa765aac6ea7f806ad66c409b7f8
Gary Pretty <gary@garypretty.co.uk>
2020-03-20 12:35:40 +0000

    Release/4.8.0 (#206)
    * Google Adapter: Google Content Type not set to UTF-8
    * Alexa Adapter: Add person to alexasystem to support Alexa voiceprint to know the ID of the speaker (#198) - Thanks @jamesemann 
    * TwitterAdapter: Added unit tests (#197) - Thanks @matiasroldan6 @santgr11 
    * DevOps: Azure DevOps Pipelines GitVersion issue
    * Dependencies: Update to Bot Framework SDK 4.8

---
+++ tests/Bot.Builder.Community.Adapters.Tests/Hosting/WebhookHostingServiceTests.cs
@@ -0,0 +1,34 @@
+﻿using Bot.Builder.Community.Adapters.Twitter.Hosting;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace Bot.Builder.Community.Adapters.Twitter.Tests.Hosting
+{
+    [TestClass]
+    [TestCategory("Twitter")]
+    public class WebhookHostingServiceTests
+    {
+        private readonly Mock<ILogger<WebhookHostedService>> _testLogger = new Mock<ILogger<WebhookHostedService>>();
+        private readonly Mock<IOptions<TwitterOptions>> _testOptions = new Mock<IOptions<TwitterOptions>>();
+        private readonly Mock<IApplicationLifetime> _testAppLifetime = new Mock<IApplicationLifetime>();
+
+        [TestMethod]
+        public void StartAsyncShouldComplete()
+        {
+            var service = new WebhookHostedService(_testAppLifetime.Object, _testOptions.Object, _testLogger.Object);
+            var start = service.StartAsync(default);
+            Assert.IsTrue(start.IsCompleted);
+        }
+
+        [TestMethod]
+        public void StopAsyncShouldComplete()
+        {
+            var service = new WebhookHostedService(_testAppLifetime.Object, _testOptions.Object, _testLogger.Object);
+            var stop = service.StopAsync(default);
+            Assert.IsTrue(stop.IsCompleted);
+        }
+    }
+}
---
+++ tests/Bot.Builder.Community.Adapters.Tests/Webhooks/Services/WebhooksEnterpriseManagerTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using Bot.Builder.Community.Adapters.Twitter.Webhooks.Services;
+using Microsoft.Extensions.Options;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace Bot.Builder.Community.Adapters.Twitter.Tests.Webhooks.Services
+{
+    [TestClass]
+    [TestCategory("Twitter")]
+    public class WebhooksEnterpriseManagerTests
+    {
+        private readonly Mock<IOptions<TwitterOptions>> _testOptions = new Mock<IOptions<TwitterOptions>>();
+
+        [TestMethod]
+        public async Task RegisterWebhookWithEmptyUrlShouldFail()
+        {
+            var enterpriseManager = new WebhooksEnterpriseManager(_testOptions.Object.Value);
+
+            await Assert.ThrowsExceptionAsync<ArgumentException>(async () => { await enterpriseManager.RegisterWebhook(string.Empty); });
+        }
+    }
+}
