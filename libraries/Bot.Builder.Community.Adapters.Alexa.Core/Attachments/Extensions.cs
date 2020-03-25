7cd58ff18b09a6be2f1a6db16af8be3adf53104c
Gary Pretty <gary@garypretty.co.uk>
2020-03-25 15:19:31 +0000

    Update attachment implementation (#213)
    * Update attachment implementation to align with BF SDK and work correctly with ABS.
    
    * Added tests for new attachment implementation

---
+++ libraries/Bot.Builder.Community.Adapters.Alexa.Core/Attachments/Extensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Alexa.NET.Response;
+using Microsoft.Bot.Schema;
+
+namespace Bot.Builder.Community.Adapters.Alexa.Core.Attachments
+{
+    public static class Extensions
+    {
+        public static Attachment ToAttachment(this ICard card)
+        {
+            return CreateAttachment(card, AlexaAttachmentContentTypes.Card);
+        }
+
+        public static Attachment ToAttachment(this IDirective directive)
+        {
+            return CreateAttachment(directive, AlexaAttachmentContentTypes.Directive);
+        }
+
+        private static Attachment CreateAttachment<T>(T card, string contentType)
+        {
+            return new Attachment
+            {
+                Content = card,
+                ContentType = contentType,
+            };
+        }
+    }
+}
