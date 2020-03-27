40761fcb352bcddf1af31bf5a9c687a90f3f3f23
Jamie D <daltskin@hotmail.com>
2020-03-27 16:14:14 +0000

    Adding RingCentral adapter (#215)
    * Adding RingCentral adapter
    
    * Updating documentation #wip
    
    * Adding packaging details
    
    * Addressing warnings
    
    Co-authored-by: Gary Pretty <gary@garypretty.co.uk>

---
+++ libraries/Bot.Builder.Community.Adapters.RingCentral/RingCentralChannelData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bot.Builder.Community.Adapters.RingCentral
+{
+    /// <summary>
+    /// Channel data that are specific to RingCentral messages.
+    /// </summary>
+    public class RingCentralChannelData
+    {
+        /// <summary>
+        /// Gets or sets id of the RingCentral source.
+        /// </summary>
+        /// <value>
+        /// The Id of the RingCentral source.
+        /// </value>
+        public string SourceId { get; set; }
+
+        /// <summary>
+        /// Gets or sets id of the RingCentral thread.
+        /// </summary>
+        /// <value>
+        /// The Id of the RingCentral thread.
+        /// </value>
+        public string ThreadId { get; set; }
+    }
+}
---
+++ libraries/Bot.Builder.Community.Adapters.RingCentral/Schema/Event.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace Bot.Builder.Community.Adapters.RingCentral.Schema
+{
+    public class Event
+    {
+        [JsonPropertyName("type")]
+        public string Type { get; set; }
+        
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+
+        [JsonPropertyName("issued_at")]
+        public DateTime IssuedDate { get; set; }
+
+        [JsonPropertyName("user_id")]
+        public string UserId { get; set; }
+
+        [JsonPropertyName("resource")]
+        public Resource Resource { get; set; }
+    }
+}
