1288c660406dffcb67fa412a1ab18832a78a9db6
Kyle Delaney <kyled@aditiconsulting.com>
2020-06-16 16:44:13 -0700

    Remove usings in AdaptiveCardTranslatorSettings

--- libraries/Bot.Builder.Community.Cards/Translation/AdaptiveCardTranslatorSettings.cs
+++ libraries/Bot.Builder.Community.Cards/Translation/AdaptiveCardTranslatorSettings.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Bot.Builder.Community.Cards.Translation;
-
-namespace Bot.Builder.Community.Cards.Translation
+﻿namespace Bot.Builder.Community.Cards.Translation
 {
     public class AdaptiveCardTranslatorSettings
     {
