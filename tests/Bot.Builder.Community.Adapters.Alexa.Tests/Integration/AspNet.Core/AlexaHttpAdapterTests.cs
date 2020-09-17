967d2736c56b6c13e40ce3b149d61f6280ae4945
Joel Mut <joel.mut@southworks.com>
2020-09-17 11:51:14 -0300

    Add unit tests for AlexaHttpAdapter class

---
+++ tests/Bot.Builder.Community.Adapters.Alexa.Tests/Integration/AspNet.Core/AlexaHttpAdapterTests.cs
@@ -0,0 +1,46 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Bot.Builder.Community.Adapters.Alexa.Integration.AspNet.Core;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Xunit;
+
+namespace Bot.Builder.Community.Adapters.Alexa.Tests.Integration.AspNet.Core
+{
+    public class AlexaHttpAdapterTests
+    {
+        private static readonly Mock<ILogger> TestLogger = new Mock<ILogger>();
+
+        [Fact]
+        public void ConstructorWithNoArgumentsShouldSucceed()
+        {
+            Assert.NotNull(new AlexaHttpAdapter());
+        }
+
+        [Fact]
+        public void ConstructorWithOptionsOnlyShouldSucceed()
+        {
+            var options = new AlexaAdapterOptions();
+            Assert.NotNull(new AlexaHttpAdapter(options));
+        }
+
+        [Fact]
+        public void ConstructorWithLoggerOnlyShouldSucceed()
+        {
+            Assert.NotNull(new AlexaHttpAdapter(null, TestLogger.Object));
+        }
+
+        [Fact]
+        public void ConstructorWithValidateRequestsOnlyShouldSucceed()
+        {
+            Assert.NotNull(new AlexaHttpAdapter(true));
+        }
+
+        [Fact]
+        public void ConstructorWithValidateRequestsAndLoggerShouldSucceed()
+        {
+            Assert.NotNull(new AlexaHttpAdapter(false, TestLogger.Object));
+        }
+    }
+}
