683d8ce2ae90bc5782080b66a3c00c277c2e4c1c
Ivan Uršić <Ivan.Ursic@infobip.com>
2020-10-20 15:03:00 +0200

    added whatsapp adapter

---
+++ libraries/Bot.Builder.Community.Adapters.Infobip.WhatsApp/Models/InfobipOmniFailoverMessage.cs
@@ -0,0 +1,16 @@
+﻿using Bot.Builder.Community.Adapters.Infobip.Core;
+using Bot.Builder.Community.Adapters.Infobip.Core.Models;
+using Newtonsoft.Json;
+
+namespace Bot.Builder.Community.Adapters.Infobip.WhatsApp.Models
+{
+    public class InfobipOmniFailoverMessage: InfobipOmniFailoverMessageBase
+    {
+        [JsonProperty("whatsApp")] public InfobipOmniWhatsAppMessage WhatsApp { get; set; }
+
+        public override string ToJson()
+        {
+            return JsonConvert.SerializeObject(this, InfobipSerialize.Settings);
+        }
+    }
+}
