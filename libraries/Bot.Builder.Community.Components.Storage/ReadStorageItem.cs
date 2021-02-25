31873b9b5d9f5877bc78ab3568b09727921f005c
Steven Ickman <stevenic@microsoft.com>
2021-02-24 19:10:00 -0800

    Added new storage component

---
+++ libraries/Bot.Builder.Community.Components.Storage/ReadStorageItem.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+using AdaptiveExpressions.Properties;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Bot.Builder.Community.Components.Storage
+{
+    public class ReadStorageItem : Dialog
+    {
+        [JsonConstructor]
+        public ReadStorageItem([CallerFilePath] string sourceFilePath = "", [CallerLineNumber] int sourceLineNumber = 0)
+            : base()
+        {
+            // enable instances of this command as debug break point
+            this.RegisterSourceLocation(sourceFilePath, sourceLineNumber);
+        }
+
+        [JsonProperty("$kind")]
+        public const string Kind = "BotBuilderCommunity.ReadStorageItem";
+
+        [JsonProperty("disabled")]
+        public BoolExpression Disabled { get; set; }
+
+        [JsonProperty("itemKey")]
+        public StringExpression ItemKey { get; set; }
+
+        [JsonProperty("resultProperty")]
+        public StringExpression ResultProperty { get; set; }
+
+        public async override Task<DialogTurnResult> BeginDialogAsync(DialogContext dc, object options = null, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            if (options is CancellationToken)
+            {
+                throw new ArgumentException($"{nameof(options)} cannot be a cancellation token");
+            }
+
+            if (this.Disabled != null && this.Disabled.GetValue(dc.State) == true)
+            {
+                return await dc.EndDialogAsync(cancellationToken: cancellationToken).ConfigureAwait(false);
+            }
+
+            var storage = dc.Context.TurnState.Get<IStorage>();
+            if (storage == null)
+            {
+                throw new Exception($"{this.Id}: storage provider could not be found in turn state.");
+            }
+
+            // Read item and convert to JObject
+            var itemKey = this.ItemKey.GetValue(dc.State);
+            var item = await storage.ReadAsync(new string[] { itemKey }, cancellationToken).ConfigureAwait(false);
+            var jItem = item.ContainsKey(itemKey) ? JToken.FromObject(item[itemKey]) : null;
+
+            // Save item
+            if (this.ResultProperty != null)
+            {
+                var resultsProp = this.ResultProperty.GetValue(dc.State);
+                dc.State.SetValue(resultsProp, jItem);
+            }
+
+            return await dc.EndDialogAsync(jItem, cancellationToken).ConfigureAwait(false);
+        }
+    }
+}
---
+++ libraries/Bot.Builder.Community.Components.Storage/StorageComponentRegistration.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using AdaptiveExpressions.Converters;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs.Debugging;
+using Microsoft.Bot.Builder.Dialogs.Declarative;
+using Microsoft.Bot.Builder.Dialogs.Declarative.Resources;
+using Newtonsoft.Json;
+
+namespace Bot.Builder.Community.Components.Storage
+{
+    /// <summary>
+    /// <see cref="StorageComponentRegistration"/> implementation for adaptive components.
+    /// </summary>
+    public class StorageComponentRegistration : ComponentRegistration, IComponentDeclarativeTypes
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StorageComponentRegistration"/> class.
+        /// </summary>
+        public StorageComponentRegistration()
+        {
+        }
+
+        /// <summary>
+        /// Gets adaptive <see cref="DeclarativeType"/> resources.
+        /// </summary>
+        /// <param name="resourceExplorer"><see cref="ResourceExplorer"/> with expected path to get all schema resources.</param>
+        /// <returns>Adaptive <see cref="DeclarativeType"/> resources.</returns>
+        public virtual IEnumerable<DeclarativeType> GetDeclarativeTypes(ResourceExplorer resourceExplorer)
+        {
+            yield return new DeclarativeType<DeleteStorageItem>(DeleteStorageItem.Kind);
+            yield return new DeclarativeType<ReadStorageItem>(ReadStorageItem.Kind);
+            yield return new DeclarativeType<WriteStorageItem>(WriteStorageItem.Kind);
+            yield break;
+        }
+
+        /// <summary>
+        /// Gets adaptive <see cref="JsonConverter"/> resources.
+        /// </summary>
+        /// <param name="resourceExplorer">ResourceExplorer to use to resolve references.</param>
+        /// <param name="sourceContext">SourceContext to build debugger source map.</param>
+        /// <returns>Adaptive <see cref="JsonConverter"/> resources.</returns>
+        public virtual IEnumerable<JsonConverter> GetConverters(ResourceExplorer resourceExplorer, SourceContext sourceContext)
+        {
+            yield break;
+        }
+    }
+}
