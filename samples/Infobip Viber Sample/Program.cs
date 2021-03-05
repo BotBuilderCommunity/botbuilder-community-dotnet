6727c75ec4ae69fbc7190a2d5af6cc082e32593c
Ivan Uršić <Ivan.Ursic@infobip.com>
2021-03-05 14:22:57 +0100

    removed microsoft headers in viber sample project

--- samples/Infobip Viber Sample/Program.cs
+++ samples/Infobip Viber Sample/Program.cs
@@ -1,7 +1,4 @@
-﻿// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-//
-// Generated with Bot Builder V4 SDK Template for Visual Studio EchoBot v4.11.1
+﻿// Generated with Bot Builder V4 SDK Template for Visual Studio EchoBot v4.11.1
 
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
--- samples/Infobip Viber Sample/Startup.cs
+++ samples/Infobip Viber Sample/Startup.cs
@@ -1,6 +1,4 @@
-﻿// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-//
+﻿
 // Generated with Bot Builder V4 SDK Template for Visual Studio EchoBot v4.11.1
 
 using Bot.Builder.Community.Adapters.Infobip.Viber;
