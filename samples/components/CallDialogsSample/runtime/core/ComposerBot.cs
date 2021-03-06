d05452b26a27a4ccb20cf2a53be5f3d3ddd9e771
Steven Ickman <stevenic@microsoft.com>
2021-03-06 10:47:51 -0800

    Update ComposerBot.cs

--- samples/components/CallDialogsSample/runtime/core/ComposerBot.cs
+++ samples/components/CallDialogsSample/runtime/core/ComposerBot.cs
@@ -1,6 +1,3 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
 using System.Security.Claims;
 using System.Security.Principal;
 using System.Threading;
