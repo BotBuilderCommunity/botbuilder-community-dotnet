2ab19e41df3ca708db2905c2271f470a9c6a6c98
Steven Ickman <stevenic@microsoft.com>
2021-03-06 10:52:00 -0800

    Update BotSettings.cs

--- samples/components/CallDialogsSample/runtime/core/Settings/BotSettings.cs
+++ samples/components/CallDialogsSample/runtime/core/Settings/BotSettings.cs
@@ -1,6 +1,3 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
 using Microsoft.ApplicationInsights.Extensibility;
 using Microsoft.Bot.Builder.Azure;
 
