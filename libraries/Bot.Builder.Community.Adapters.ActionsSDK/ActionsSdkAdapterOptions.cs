0a8292df78c2ae6e3fa6150937498eef320240a8
carlosscastro <ccastro@microsoft.com>
2021-03-16 21:34:25 -0700

    Adapters: add data annotation validations for adapter component

--- libraries/Bot.Builder.Community.Adapters.ActionsSDK/ActionsSdkAdapterOptions.cs
+++ libraries/Bot.Builder.Community.Adapters.ActionsSDK/ActionsSdkAdapterOptions.cs
@@ -1,11 +1,15 @@
-﻿namespace Bot.Builder.Community.Adapters.ActionsSDK
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bot.Builder.Community.Adapters.ActionsSDK
 {
     public class ActionsSdkAdapterOptions
     {
         public bool ShouldEndSessionByDefault { get; set; } = true;
 
+        [Required]
         public string ActionInvocationName { get; set; }
 
+        [Required]
         public string ActionProjectId { get; set; }
     }
 }
