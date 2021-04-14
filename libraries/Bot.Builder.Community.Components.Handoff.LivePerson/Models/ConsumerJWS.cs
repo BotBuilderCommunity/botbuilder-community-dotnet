e3bd85061a45552193c4f397e1ab588664fe008d
Gary Pretty <gary@garypretty.co.uk>
2021-04-14 17:58:11 +0100

    Add LivePerson Handoff Package and Handoff Shared package

---
+++ libraries/Bot.Builder.Community.Components.Handoff.LivePerson/Models/ConsumerJWS.cs
@@ -0,0 +1,7 @@
+﻿namespace Bot.Builder.Community.Components.Handoff.LivePerson.Models
+{
+    public class ConsumerJWS
+    {
+        public string token { get; set; }
+    }
+}
---
+++ libraries/Bot.Builder.Community.Components.Handoff.LivePerson/Models/WebhookData.cs
@@ -0,0 +1,9 @@
+﻿namespace Bot.Builder.Community.Components.Handoff.LivePerson.Models
+{
+    public class WebhookData
+    {
+        public string kind { get; set; }
+        public Webhook.Body body { get; set; }
+        public string type { get; set; }
+    }
+}
