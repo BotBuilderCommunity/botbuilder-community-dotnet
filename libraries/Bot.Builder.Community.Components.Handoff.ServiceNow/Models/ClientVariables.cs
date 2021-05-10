e06b3ca536eef25b5a2aefac407b3032231c4844
Darren Jefford <33519520+darrenj@users.noreply.github.com>
2021-05-10 18:10:30 +0100

    ServiceNow Handoff Implementation (#388)
    * Add LivePerson handoff component. Tidy up package references and shared NuGet props.
    
    * Update SDK / preview targets
    
    * First implementation of ServiceNow handoff utilising a fixed user/password for demonstration purposes. Further work is identified to identify pass-through authentication (SSO)
    
    * adding oauth config setting and trace
    
    * oauth work
    
    * oAuth implementation and readme
    
    * new response handling
    
    Co-authored-by: Gary Pretty <gary@garypretty.co.uk>

---
+++ libraries/Bot.Builder.Community.Components.Handoff.ServiceNow/Models/ClientVariables.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bot.Builder.Community.Components.Handoff.ServiceNow.Models
+{
+    public class ClientVariables
+    {
+        public string conversationId { get; set; }
+    }
+}
---
+++ libraries/Bot.Builder.Community.Components.Handoff.ServiceNow/Models/Option.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bot.Builder.Community.Components.Handoff.ServiceNow.Models
+{
+    public class Option
+    {
+        public string label { get; set; }
+        public string description { get; set; }
+        public string attachment { get; set; }
+        public string value { get; set; }
+        public bool enabled { get; set; }
+    }
+}
