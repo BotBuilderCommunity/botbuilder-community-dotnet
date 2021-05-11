8ee1370c551304f7f16e008cea1daab001973a44
Eric Dahlvang <erdahlva@microsoft.com>
2021-05-11 01:08:57 -0700

    Add sso token exchange skill handler package (#387)
    * Add Bot.Builder.Community.Components.TokenExchangeSkillHandler
    
    * Remove package generation from TokenExchangeSkillHandler project
    
    * Readme fix
    
    * Bot.Builder.Community.Components.TokenExchangeSkillHandler readme update
    
    * Add Prerequisite to readme
    
    * Add null checks to SkillsConfiguration
    
    * Add SkillConfigurationTests

---
+++ libraries/Bot.Builder.Community.Components.TokenExchangeSkillHandler/TokenExchangeComponent.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.Bot.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Bot.Builder.Community.Components.TokenExchangeSkillHandler
+{
+    /// <summary>
+    /// <see cref="BotComponent"/> for registering the TokenExchangeHandler with the ServicesCollection.
+    /// </summary>
+    public class TokenExchangeComponent : BotComponent
+    {
+        /// <inheritdoc/>
+        public override void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+        {
+            if (services == null)
+            {
+                throw new ArgumentNullException(nameof(services));
+            }
+
+            if (configuration == null)
+            {
+                throw new ArgumentNullException(nameof(configuration));
+            }
+
+            var settings = configuration.Get<ComponentSettings>() ?? new ComponentSettings();
+            if (settings.UseTokenExchangeSkillHandler)
+            {
+                services.AddSingleton<ChannelServiceHandlerBase, TokenExchangeSkillHandler>();
+            }
+        }
+    }
+}
