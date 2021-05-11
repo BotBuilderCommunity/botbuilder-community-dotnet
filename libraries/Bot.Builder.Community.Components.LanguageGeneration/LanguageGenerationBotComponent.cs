b28b474c0b3409654310e5900d5a4937b14a5e56
Steven Ickman <stevenic@microsoft.com>
2021-05-11 13:57:29 -0700

    New LG components for manually evaluating an LG template (#394)

---
+++ libraries/Bot.Builder.Community.Components.LanguageGeneration/LanguageGenerationBotComponent.cs
@@ -0,0 +1,19 @@
+﻿using AdaptiveExpressions.Converters;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs.Declarative;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Bot.Builder.Community.Components.LanguageGeneration
+{
+    /// <summary>
+    /// <see cref="BotComponent"/> implementation for the storage actions.
+    /// </summary>
+    public class LanguageGenerationBotComponent : BotComponent
+    {
+        public override void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddSingleton<DeclarativeType>(new DeclarativeType<EvaluateLGTemplate>(EvaluateLGTemplate.Kind));
+        }
+    }
+}
---
+++ libraries/Bot.Builder.Community.Components.LanguageGeneration/LanguageGenerationComponentRegistration.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Bot.Builder.Dialogs.Declarative.Obsolete;
+
+namespace Bot.Builder.Community.Components.LanguageGeneration
+{
+    /// <summary>
+    /// <see cref="LanguageGenerationComponentRegistration"/> implementation for adaptive components.
+    /// </summary>
+    public class LanguageGenerationComponentRegistration : DeclarativeComponentRegistrationBridge<LanguageGenerationBotComponent>
+    {
+    }
+}
