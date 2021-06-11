70745a6daa07e6c0c5f9d3a29fd59771c6a83287
Arafat Tehsin <arafattehsin@hotmail.com>
2021-06-11 22:42:54 +1000

    Add Sentiment Analysis Middleware Component (#400)
    * chore: update the SentimentAnalyzer reference for Sentiment Analysis Middleware
    
    * chore: add the Sentiment Analysis Middleware component
    
    * chore: modified the description.
    
    * Update README.md
    
    Add all the relevant details
    
    * chore: add solution file and fixed the null check
    
    * chore: add it to the turn state and remove from conversation state after discussing with SDK team
    
    Co-authored-by: Gary Pretty <gary@garypretty.co.uk>

---
+++ libraries/Bot.Builder.Community.Components.Middleware.SentimentAnalysis/SentimentMiddleware.cs
@@ -0,0 +1,34 @@
+﻿using Bot.Builder.Community.Components.Middleware.SentimentAnalysis.Models;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Schema;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Bot.Builder.Community.Components.Middleware.SentimentAnalysis
+{
+    public class SentimentMiddleware : IMiddleware
+    {
+        private readonly ISentimentAnalysisCredentialsProvider _creds;
+        private const string Turn = "turn";
+
+        public SentimentMiddleware(ISentimentAnalysisCredentialsProvider credentialsProvider)
+        {
+            _creds = credentialsProvider;
+        }
+
+        public string ApiKey { get; }
+
+        public async Task OnTurnAsync(ITurnContext turnContext, NextDelegate next, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            if (turnContext.Activity.Type is ActivityTypes.Message && _creds.IsEnabled == true)
+            {
+                ObjectPath.SetPathValue(turnContext.TurnState, "turn.Sentiment", await turnContext.Activity.Text.Sentiment(_creds.APIKey, _creds.EndpointUrl));
+            }
+
+            await next(cancellationToken);
+        }
+    }
+}
