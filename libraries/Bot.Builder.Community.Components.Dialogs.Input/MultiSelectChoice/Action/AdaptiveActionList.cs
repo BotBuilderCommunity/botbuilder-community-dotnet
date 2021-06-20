28c6ae870cddc5c0703e22cd98a7b1a3adf3df73
rvinothrajendran <r.vinoth@live.com>
2021-06-20 10:46:44 +0200

    Added MultiSelect choice input type

---
+++ libraries/Bot.Builder.Community.Components.Dialogs.Input/MultiSelectChoice/Action/AdaptiveActionList.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using AdaptiveCards;
+
+namespace Bot.Builder.Community.Components.Dialogs.Input.MultiSelectChoice.Action
+{
+    public class AdaptiveActionList : IAdaptiveAction
+    {
+        public List<AdaptiveAction> PrepareActionList(string captionName)
+        {
+            var adaptiveActions = new List<AdaptiveAction>
+            {
+                new AdaptiveSubmitAction()
+                {
+                    Type = "Action.Submit",
+                    Title = captionName
+                }
+            };
+
+            return adaptiveActions;
+        }
+    }
+}
