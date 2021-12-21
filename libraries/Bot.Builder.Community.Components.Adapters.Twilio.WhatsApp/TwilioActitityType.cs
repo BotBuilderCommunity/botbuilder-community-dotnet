0b5b05fb7cb745b01f117eb533c3a0e10b8bbbf9
rvinothrajendran <r.vinoth@live.com>
2021-12-21 15:58:50 +0100

    Added Twilio WhatsApp adapter for the Microsoft Bot Framework & Composer

---
+++ libraries/Bot.Builder.Community.Components.Adapters.Twilio.WhatsApp/TwilioActitityType.cs
@@ -0,0 +1,12 @@
+﻿namespace Bot.Builder.Community.Components.Adapters.Twilio.WhatsApp
+{
+    public class TwilioWhatsAppActivityTypes
+    {
+        public const string MessageRead = "messageRead";
+        public const string MessageDelivered = "messageDelivered";
+        public const string MessageSent = "messageSent";
+        public const string MessageQueued = "messageQueued";
+        public const string MessageFailed = "messageFailed";
+    }
+
+}
