62b22704e29a28a05f21105fed08f67189e6edd0
Vinoth Rajendran <r.vinoth@live.com>
2022-01-21 15:02:23 +0100

    Expire Conversation Trigger for Bot composer (#456)
    * Added expire conversation trigger
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    Co-authored-by: Gary Pretty <gary@garypretty.co.uk>

---
+++ libraries/Bot.Builder.Community.Components.Trigger.ExpireConversation/Trigger/OnExpireConversation.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using AdaptiveExpressions;
+using Bot.Builder.Community.Components.Trigger.ExpireConversation.Middleware;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Dialogs.Adaptive.Conditions;
+using Newtonsoft.Json;
+
+namespace Bot.Builder.Community.Components.Trigger.ExpireConversation.Trigger
+{
+    public class OnExpireConversation : OnActivity
+    {
+        [JsonProperty("$kind")]
+        public new const string Kind = "OnExpireConversation";
+
+        [JsonProperty("ExpireAfterSeconds")]
+        public int ExpireAfterSeconds { get; set; }
+
+        [JsonConstructor]
+        public OnExpireConversation([CallerFilePath] string callerPath = "", [CallerLineNumber] int callerLine = 0)
+            : base(type: ConversationActivity.ActivityName, callerPath: callerPath, callerLine: callerLine)
+        {
+            
+        }
+
+        protected override Expression CreateExpression()
+        {
+            Type = ConversationActivity.ActivityName;
+
+            if (ExpireAfterSeconds > 0)
+            {
+                var expireSeconds = Convert.ToDouble(ExpireAfterSeconds);
+                ExpireConversationMiddleware.ExpireAfterSeconds = expireSeconds;
+            }
+
+            return Expression.AndExpression(Expression.Parse($"{TurnPath.Activity}.Type == '{this.Type}'"), base.CreateExpression());
+        }
+    }
+}
