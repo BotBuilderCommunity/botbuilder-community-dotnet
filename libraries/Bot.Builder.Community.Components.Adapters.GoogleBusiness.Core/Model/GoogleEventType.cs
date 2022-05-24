b9526356a6a48b10aa99d4e2a3e9fab60ac2e3f7
Zhipeng Wang <zhiwang@microsoft.com>
2022-05-24 16:22:24 +0800

    Add activity to google event for GoogleBusinessMessagingRequestMapper (#483)
    * Add activity to google event mapping
    
    * update display name

---
+++ libraries/Bot.Builder.Community.Components.Adapters.GoogleBusiness.Core/Model/GoogleEventType.cs
@@ -0,0 +1,11 @@
+﻿namespace Bot.Builder.Community.Components.Adapters.GoogleBusiness.Core.Model
+{
+    public enum GoogleEventType
+    {
+        EVENT_TYPE_UNSPECIFIED,
+        TYPING_STARTED,
+        TYPING_STOPPED,
+        REPRESENTATIVE_JOINED,
+        REPRESENTATIVE_LEFT
+    }
+}
---
+++ libraries/Bot.Builder.Community.Components.Adapters.GoogleBusiness.Core/Model/OutgoingEvent.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace Bot.Builder.Community.Components.Adapters.GoogleBusiness.Core.Model
+{
+    public class OutgoingEvent
+    {
+        [JsonConverter(typeof(StringEnumConverter))]
+        public GoogleEventType EventType { get; set; }
+
+        public Representative Representative { get; set; } = new Representative()
+        {
+            DisplayName = "Bot",
+            RepresentativeType = "BOT"
+        };
+    }
+}
