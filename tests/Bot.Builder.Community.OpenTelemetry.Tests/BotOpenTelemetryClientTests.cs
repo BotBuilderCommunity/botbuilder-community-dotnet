6583d97525961e7fd62e330472a3be8c15b40336
Sébastien BIAUDET <sebastien.biaudet@gmail.com>
2022-11-08 12:50:41 +0100

    Back test to netcoreapp3.1

--- tests/Bot.Builder.Community.OpenTelemetry.Tests/BotOpenTelemetryClientTests.cs
+++ tests/Bot.Builder.Community.OpenTelemetry.Tests/BotOpenTelemetryClientTests.cs
@@ -9,6 +9,9 @@ using OpenTelemetry.Resources;
 using OpenTelemetry.Trace;
 using System;
 using System.Diagnostics.Metrics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Bot.Builder.Community.OpenTelemetry.Tests
 {
